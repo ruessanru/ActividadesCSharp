319835d3a09d6a9a48db691342f747f1e8cce6e3
ruessanru <rssrsanru@gmail.com>
2023-07-06 18:03:02 -0500

    actividad 1

---
+++ Actividad_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Escriba su Nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("hola "+ nombre);
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine(datetime);
+        
