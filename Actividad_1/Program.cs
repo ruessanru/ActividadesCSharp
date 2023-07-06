665e087869a9a46fa77f3752f55cf80ad7dee745
ruessanru <rssrsanru@gmail.com>
2023-07-06 18:22:52 -0500

    Se colocaron comentarios a la actividad

--- Actividad_1/Program.cs
+++ Actividad_1/Program.cs
@@ -1,8 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Ejercicio 1
 
 Console.WriteLine("Escriba su Nombre:");
 string nombre = Console.ReadLine();
 Console.WriteLine("hola "+ nombre);
+
+//Ejercicio 2
 
 string datetime = DateTime.Now.ToString("hh:mm:ss tt");
 Console.WriteLine(datetime);
